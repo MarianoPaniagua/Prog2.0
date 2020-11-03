3fb9388a96b8e8656db1885df4672203d8d7c1dc
Mariano Paniagua <mpaniagua@contractor.ea.com>
2020-11-03 18:31:09 -0300

    refactors

--- src/main/java/pocion/PocionModifica.java
+++ src/main/java/pocion/PocionModifica.java
@@ -8,28 +8,24 @@ public class PocionModifica extends Pocion {
     Atributo atributoDeLaPocion;
 
     public PocionModifica(String nombre, Atributo atributoDeLaPocion) {
-        super(nombre,false);
+        super(nombre, false);
         this.atributoDeLaPocion = atributoDeLaPocion;
     }
 
     @Override
     public Carta aplicarPocion(Carta carta, String atributoCarta) {
-        if(!this.aplicada) {
-            if (atributoDeLaPocion.getNombreAtributo() == null) {
-                for (Atributo atributo : carta.getAtributos()) {
+        if (!this.aplicada) {
+            for (Atributo atributo : carta.getAtributos()) {
+                if (atributoDeLaPocion.getNombreAtributo() == null || atributo.getNombreAtributo().equals(atributoDeLaPocion.getNombreAtributo())) {
                     carta.reemplazarAtributo(atributo.getNombreAtributo(), atributoDeLaPocion.getValor());
                 }
-                System.out.println("Se aplicó la pocima " + this.nombre +
-                        " , el valor resultante es " + carta.getAtributo(atributoCarta).getValor());
-                return carta;
-            } else if (atributoCarta.equals(this.atributoDeLaPocion.getNombreAtributo())) {
-                carta.reemplazarAtributo(atributoDeLaPocion.getNombreAtributo(), atributoDeLaPocion.getValor());
-                System.out.println("Se aplicó la pocima " + this.nombre +
-                        " , el valor resultante es " + carta.getAtributo(atributoCarta).getValor());
             }
+        }
+        if (!this.aplicada && (atributoDeLaPocion.getNombreAtributo() == null || atributoCarta.equals(atributoDeLaPocion.getNombreAtributo()))) {
+            System.out.println("Se aplicó la pocima " + this.nombre +
+                    " , el valor resultante es " + carta.getAtributo(atributoCarta).getValor());
         }
         this.aplicada = true;
         return carta;
     }
-
 }
